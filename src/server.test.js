1662014d4262de80d9301148c6f3e44f49ee4c5e
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2025-11-26 16:48:04 +0530

    Test CI/CD - 2

--- src/server.test.js
+++ src/server.test.js
@@ -5,6 +5,6 @@ describe("GET /", function () {
   it("should return hello world", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe("Hello World!");
+    expect(res.text).toBe("Hello World Test!");
   });
 });
